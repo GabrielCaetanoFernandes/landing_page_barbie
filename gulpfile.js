59e88171ed2e065e6200e679046f389d0ce007b2
Gabriel Caetano <gabriel.caetano9@icloud.com>
2023-08-11 21:15:28 -0300

    commit inicial do projeto

---
+++ gulpfile.js
@@ -0,0 +1,31 @@
+const gulp = require('gulp');
+const imagemin = require('gulp-imagemin');
+const sass = require('gulp-sass')(require('sass'));
+const maps = require('gulp-sourcemaps');
+const uglify = require('gulp-uglify');
+
+function sassCompiler(){
+    return gulp.src('./src/style/*.scss')
+    .pipe(sourcemaps.init())
+    .pipe(sass({
+        outputStyle: 'compressed'
+    }))
+    .pipe(sourcemaps.write('./maps'))
+    .pipe(gulp.dest('./build/styles'));
+}
+function imgCompress(){
+    return gulp.src('./src/images/*')
+    .pipe(imagemin())
+    .pipe(gulp.dest('./build/images'));
+}
+function jsCompress(){
+    return gulp.src('./src/scripts/*.js')
+    .pipe(uglify())
+    .pipe(gulp.dest('./build/scripts'));
+}
+
+exports.default = function(){
+    gulp.watch('./src/style/*.scss'), {ignoreInitial: false}, gulp.series(sassCompiler);
+    gulp.watch('./src/scripts/main.js'), {ignoreInitial: false}, gulp.series(jsCompress);
+    gulp.watch('./src/images/*'), {ignoreInitial: false}, gulp.series(imgCompress);
+}
