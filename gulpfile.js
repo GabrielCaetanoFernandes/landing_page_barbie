215844deeb32db9c8942598dad369310d1a6da23
Gabriel Caetano <caetano9650@gmail.com>
2023-08-15 14:27:57 -0300

    Criação do header

--- gulpfile.js
+++ gulpfile.js
@@ -1,21 +1,15 @@
 const gulp = require('gulp');
-const imagemin = require('gulp-imagemin');
 const sass = require('gulp-sass')(require('sass'));
-const maps = require('gulp-sourcemaps');
+const imagemin = require('gulp-imagemin');
 const uglify = require('gulp-uglify');
-const watch = require('gulp-watch')
 
 function sassCompiler(){
     return gulp.src('./src/style/*.scss')
-    .pipe(sourcemaps.init())
-    .pipe(sass({
-        outputStyle: 'compressed'
-    }))
-    .pipe(sourcemaps.write('./maps'))
+    .pipe(sass({outputStyle: 'compressed'}))
     .pipe(gulp.dest('./build/styles'));
 }
 function imgCompress(){
-    return gulp.src('./src/images/*')
+    return gulp.src('./src/images/**/*')
     .pipe(imagemin())
     .pipe(gulp.dest('./build/images'));
 }
@@ -25,10 +19,10 @@ function jsCompress(){
     .pipe(gulp.dest('./build/scripts'));
 }
 
-exports.default = gulp.parallel(sassCompiler, imgCompress, jsCompress);
+exports.default = exports.default = gulp.parallel(sassCompiler, imgCompress, jsCompress);
 
 exports.watch = function(){
-    gulp.watch('./src/style/*.scss'), {ignoreInitial: false}, gulp.series(sassCompiler);
-    //gulp.watch('./src/scripts/main.js'), {ignoreInitial: false}, gulp.series(jsCompress);
-    gulp.watch('./src/images/*'), {ignoreInitial: false}, gulp.series(imgCompress);
+    gulp.watch('./src/styles/main.scss', gulp.parallel(sassCompiler))
+    gulp.watch('./src/scripts/*.js', gulp.parallel(jsCompress))
 }
+//gulp.parallel(sassCompiler, imgCompress, jsCompress);
